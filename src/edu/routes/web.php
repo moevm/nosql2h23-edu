a3751a6dc8a975e96706945e03a2d231663d3b50
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2023-11-23 16:22:33 +0300

    refs #2: created user model to test connection to db

--- src/edu/routes/web.php
+++ src/edu/routes/web.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/test', function () {
-   $user = new User();
-   $user->save();
-});
