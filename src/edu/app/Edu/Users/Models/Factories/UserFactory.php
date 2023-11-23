2b4c56ad5d03f64b54ecbaba029eb1f4acc3456c
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2023-11-24 00:30:05 +0300

    refs #3: created user factory

---
+++ src/edu/app/Edu/Users/Models/Factories/UserFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Edu\Users\Models\Factories;
+
+use App\Edu\Users\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserFactory
+{
+    public static function create(array $attributes): User
+    {
+        $user = new User();
+
+        $user->email = $attributes['email'];
+        $user->password = Hash::make($attributes['password']);
+        $user->name = $attributes['name'];
+        $user->surname = $attributes['surname'];
+        $user->patronymic = $attributes['patronymic'];
+        $user->date_birth = $attributes['date_birth'];
+        $user->gender = $attributes['gender'];
+
+        $savingResult = $user->save();
+        if (!$savingResult) {
+            throw new \RuntimeException('Could not create user');
+        }
+
+        return $user;
+    }
+}
