9aa62fe0fe5f06c73ec2c41f45658942aa56ef74
Anatoliy Denezhniy <a.denezhnyy@crpo.su>
2023-11-25 23:52:32 +0300

    refs #4: update validation for create user form request

--- src/edu/app/Edu/Users/Http/Requests/CreateUserRequest.php
+++ src/edu/app/Edu/Users/Http/Requests/CreateUserRequest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Edu\Users\Http\Requests;
 
+use App\Edu\Roles\Enums\AvailableRoles;
+use App\Edu\Users\Enums\UserGender;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CreateUserRequest extends FormRequest
 {
@@ -16,9 +19,28 @@ class CreateUserRequest extends FormRequest
             'surname' => 'required|alpha',
             'patronymic' => 'required|alpha',
             'date_birth' => 'required|date',
-            'gender' => 'required|integer|in:0,1',
             'password_confirmation' => 'required',
             'password' => 'required|confirmed',
+            'gender' => $this->getGenderValidationRules(),
+            'role_title' => $this->getRoleTitleValidationRules(),
+        ];
+    }
+
+    private function getRoleTitleValidationRules(): array
+    {
+        return [
+            'required',
+            'string',
+            Rule::in(getEnumValues(AvailableRoles::class)),
+        ];
+    }
+
+    private function getGenderValidationRules(): array
+    {
+        return [
+            'required',
+            'integer',
+            Rule::in(getEnumValues(UserGender::class))
         ];
     }
 }
