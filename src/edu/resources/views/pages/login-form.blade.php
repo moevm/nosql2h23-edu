29133b5bc55111887e7b471f8b20417b2161cc35
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-02 13:29:25 +0200

    Add needed obj

--- src/edu/resources/views/pages/login-form.blade.php
+++ src/edu/resources/views/pages/login-form.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+</head>
+<body>
+<div class="container-center">
+    <h1>Войти в систему</h1>
+    <div class="container">
+        <h2>Для работы системы необходимо авторизоваться.
+            Пожалуйста, введите логин и пароль.</h2>
+        <form>
+            <input type="text" placeholder="Логин">
+            <input type="password" placeholder="Пароль">
+        </form>
+        <p><a href="#" class="link-registration">Регистрация</a></p>
+        <p><a href="#" class="link-forget-password">Забыли пароль?</a></p>
+        <button type="button-signin" class="btn">Войти</button>
+    </div>
 </div>
+</body>
+</html>
