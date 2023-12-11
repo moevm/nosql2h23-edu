84c7e506a843715e0d7e9a9fcb13c4fa0dee0f26
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-11 13:29:35 +0200

    create Migration

--- src/edu/database/migrations/2023_12_11_112745_creating_default_users.php
+++ src/edu/database/migrations/2023_12_11_112745_creating_default_users.php
@@ -1,24 +1,58 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Edu\Roles\Enums\AvailableRoles;
+use App\Edu\Users\Enums\UserGender;
+use App\Edu\Users\Factories\UserFactory;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    private const PASSWORD = '123456';
+
     public function up(): void
     {
-        //
+        $adminAttributes = [
+            'email' => 'tolya@mail.ru',
+            'password' => static::PASSWORD,
+            'name' => 'Анатолий',
+            'surname' => 'Денежный',
+            'patronymic' => 'Антонович',
+            'date_birth' => '2002-07-17',
+            'gender' => UserGender::MALE->value,
+            'role_title' => AvailableRoles::ADMIN->value,
+        ];
+        $firstUserAttributes = [
+            'email' => 'liza@mail.ru',
+            'password' => static::PASSWORD,
+            'name' => 'Елизавета',
+            'surname' => 'Костебелова',
+            'patronymic' => 'Константиновна',
+            'date_birth' => '2002-14-09',
+            'gender' => UserGender::FEMALE->value,
+            'role_title' => AvailableRoles::REGULAR_USER->value,
+        ];
+        $secondUserAttributes = [
+            'email' => 'katya@mail.ru',
+            'password' => static::PASSWORD,
+            'name' => 'Екатерина',
+            'surname' => 'Курочкина',
+            'patronymic' => 'Александровна',
+            'date_birth' => '2002-07-10',
+            'gender' => UserGender::FEMALE->value,
+            'role_title' => AvailableRoles::REGULAR_USER->value,
+        ];
+
+        $usersAttributes = [$adminAttributes, $firstUserAttributes, $secondUserAttributes];
+        foreach ($usersAttributes as $attributes) {
+            UserFactory::create($attributes);
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::connection('mongodb')->dropIfExists('users');
     }
 };
