7b1fc0aa43850efe0875d76c8551f184c69a2401
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-11 13:31:16 +0200

    create Migration for course

--- src/edu/database/migrations/2023_12_11_113009_creating_default_courses.php
+++ src/edu/database/migrations/2023_12_11_113009_creating_default_courses.php
@@ -1,24 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Edu\Courses\Factories\CourseFactory;
+use App\Edu\Roles\Enums\AvailableRoles;
+use App\Edu\Roles\Models\Role;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        /** @var Role $adminRole */
+        $adminRole = Role::query()
+            ->where('title', '=', AvailableRoles::ADMIN->value)
+            ->first();
+
+        $coursesAuthor = $adminRole->users->first();
+        if (!$coursesAuthor) {
+            throw new \DomainException('Admin was not found');
+        }
+
+        $firstCourse = [
+            'title' => 'Основы подготовки научных публикаций',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+            'course_author' => $coursesAuthor,
+        ];
+        $secondCourse = [
+            'title' => 'Введение в нереляционные базы данных',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+            'course_author' => $coursesAuthor,
+        ];
+
+        $courses = [$firstCourse, $secondCourse];
+        foreach ($courses as $course) {
+            CourseFactory::create($course);
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::connection('mongodb')->dropIfExists('courses');
     }
 };
