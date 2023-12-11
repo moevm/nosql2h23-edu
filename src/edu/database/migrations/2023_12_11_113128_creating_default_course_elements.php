af090b74fb9658eaf1ac88e5a9c10ec756369da4
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-11 13:32:15 +0200

    create Migration for elements

--- src/edu/database/migrations/2023_12_11_113128_creating_default_course_elements.php
+++ src/edu/database/migrations/2023_12_11_113128_creating_default_course_elements.php
@@ -1,24 +1,44 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Edu\Courses\Models\Course;
+use App\Edu\Elements\Enums\AvailableElementTypes;
+use App\Edu\Elements\Factories\ElementFactory;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        $courses = Course::all();
+
+        foreach ($courses as $course) {
+            $firstElement = [
+                'type' => AvailableElementTypes::TYPE_TEXT->value,
+                'title' => 'Интересный текст',
+                'content' => 'Привет, это очень интересный текст',
+                'weight' => 1.0,
+                'course' => $course,
+            ];
+            $secondElement = [
+                'type' => AvailableElementTypes::TYPE_LINK->value,
+                'title' => 'Важная ссылка',
+                'content' => 'https://se.moevm.info/doku.php/staff:courses:no_sql_introduction',
+                'weight' => 1.0,
+                'course' => $course,
+            ];
+
+            $courseElements = [$firstElement, $secondElement];
+            foreach ($courseElements as $courseElement) {
+                ElementFactory::create($courseElement);
+            }
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::connection('mongodb')->dropIfExists('elements');
     }
 };
