0db99071c5251f77804c72d28a52681e4fd79565
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-11 13:04:53 +0200

    add class body

--- src/edu/app/Edu/Elements/DTO/ElementsFilterDTO.php
+++ src/edu/app/Edu/Elements/DTO/ElementsFilterDTO.php
@@ -1,8 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Edu\Elements\DTO;
 
 class ElementsFilterDTO
 {
+    private ?string $elementType = null;
 
+    private ?string $elementTitle = null;
+
+    public function setElementType(?string $type): self
+    {
+        $this->elementType = $type;
+
+        return $this;
+    }
+
+    public function setElementTitle(?string $title): self
+    {
+        $this->elementTitle = $title;
+
+        return $this;
+    }
+
+    public function getElementType(): ?string
+    {
+        return $this->elementType;
+    }
+
+    public function getElementTitle(): ?string
+    {
+        return $this->elementTitle;
+    }
 }
