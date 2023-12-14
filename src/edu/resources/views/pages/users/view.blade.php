86d91238adb40f1160f6586ff20544d51c73aaa6
Kostebelova-Elizaveta <Lizbet227@gmail.com>
2023-12-14 19:47:30 +0200

    Change some front-view and connect buttons and links with back-end

--- src/edu/resources/views/pages/users/view.blade.php
+++ src/edu/resources/views/pages/users/view.blade.php
@@ -9,33 +9,42 @@
 <h1>Редактировать профиль</h1>
 <div class="container-center">
     <h2>Персональная информация</h2>
-    <p>
-        <a class="name">Имя <a class="red-star">*</a></a>
-        <a class="birthday">Дата рождения <a class="red-star">*</a></a>
-    </p>
-    <form>
-        <input class="name-enter" type="text">
-        <input class="birth-enter" type="date">
+    <form method="POST" action="{{ route('users.user.edit', ['userId' => $user->getKey()]) }}" class="links">
+        @csrf
+        <label>
+            <p> Email</p>
+            <input name="email" type="email" class="login-enter" value="{{ $user->email }}" required>
+        </label>
+        <label>
+            <p> Имя<a class="red-star">*</a></p>
+            <input name="name" type="text" class="name-enter" value="{{ $user->name }}" required>
+        </label>
+        <label>
+            <p> Фамилия<a class="red-star">*</a></p>
+            <input name="surname" type="text" class="surname-enter" value="{{ $user->surname }}" required>
+        </label>
+        <label>
+            <p> Отчество</p>
+            <input name="patronymic" type="text" class="patronymic-enter" value="{{ $user->patronymic }}" required>
+        </label>
+        <label>
+            <p> Дата рождения<a class="red-star">*</a></p>
+            <input name="date_birth" type="date" class="birth-enter" value="{{ $user->date_birth }}" required>
+        </label>
+        <label>
+            <p> Выберите пол<a class="red-star">*</a></p>
+        </label>
+        <label>
+            <p> Мужской</p>
+            <input name="gender" type="radio" class="gender-enter" value="1" required>
+        </label>
+        <label>
+            <p> Женский</p>
+            <input name="gender" type="radio" class="gender-enter" value="0" required>
+        </label>
+        <p class="note">Чтобы сохранить изменения нажмите "Сохранить".</p>
+        <button type="submit" class="btn-save">Сохранить</button>
     </form>
-    <p>
-        <a class="sername">Фамилия <a class="red-star">*</a></a>
-        <a class="sex">Пол <a class="red-star">*</a></a>
-    </p>
-    <form>
-        <div class="special-group" role="group">
-            <input class="sername-enter" type="text">
-            <button type="button" class="btn-male">Мужской</button>
-            <button type="button" class="btn-female">Женский</button>
-        </div>
-    </form>
-    <p class="patr">Отчество</p>
-    <input class="patr-enter" type="text">
-
-    <p class="note">Чтобы сохранить изменения нажмите "Сохранить".</p>
-    <div class="btn-group" role="group">
-        <button type="button" class="btn-back-to-users">К списку пользователей</button>
-        <button type="button" class="btn-save">Сохранить</button>
-    </div>
 </div>
 </body>
 </html>
@@ -52,7 +61,7 @@
     div.container-center {
         background: white;
         width: 1000px;
-        height: 550px;
+        text-align: center;
     }
     p {
         font-size: 24px;
@@ -80,15 +89,6 @@
         margin-top: 50px;
         color: #2E6243;
     }
-    button.btn-male {
-        margin-left: 50px;
-    }
-    button.btn-female {
-        margin-left: -6px;
-    }
-    input.birth-enter {
-        margin-left: 50px;
-    }
     button.btn-back-to-users, button.btn-save {
         font-size: 24px;
         color: #FFFFFF;
@@ -96,24 +96,5 @@
     }
     button.btn-back-to-users:hover, button.btn-save:hover {
         opacity: 80%;
-    }
-    h2 {
-        text-align: center;
-        margin-top: 10px;
-    }
-    a.name, a.sername, p.patr {
-        margin-left: 10px;
-    }
-    input.name-enter, input.sername-enter, input.patr-enter {
-        margin-left: 10px;
-    }
-    a.sex {
-        margin-left: 310px;
-    }
-    a.birthday {
-        margin-left: 370px;
-    }
-    div.btn-group {
-        text-align: right;
     }
 </style>
