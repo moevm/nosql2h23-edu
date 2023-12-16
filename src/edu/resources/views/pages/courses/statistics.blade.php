1efdca01092e81ab014e1a7c9d34acce5cbb7df6
Ekaterina0303 <katya74246@mail.ru>
2023-12-16 17:53:50 +0300

    fix statistics page

---
+++ src/edu/resources/views/pages/courses/statistics.blade.php
@@ -0,0 +1,143 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Статистика по курсам</title>
+</head>
+<body>
+<h1>Статистика по курсам</h1>
+<div class="container-top">
+    <h2>
+        <a href="{{ route('courses.statistics') }}">Сбросить фильтры</a>
+    </h2>
+
+    <form method="GET" action="{{ route('courses.statistics') }}">
+        <input name="filters[assignments_count]" class="input-name" type="text" placeholder="Количество обучающихся более">
+        <input name="filters[passed_count]" class="input-surname" type="text" placeholder="Количество прошедших курс более">
+        <button type="submit" class="btn-search">Поиск</button>
+    </form>
+
+    <a href="{{ route("courses.export-statistics") }}" class="a-download-json">Выгрузить в json формате</a>
+    <form method="POST" action="{{ route("courses.import") }}" enctype="multipart/form-data">
+        @csrf
+        <input type="file" name="file">
+        <button type="submit">Загрузить в json формате</button>
+    </form>
+
+</div>
+
+<div class="container-bot">
+    <table class="table-users">
+        <thead>
+        <th class="td-title">Название</th>
+        <th class="td-assignments">Количество обучающихся</th>
+        <th class="td-passed">Количество прошедших курс</th>
+        </thead>
+        <tbody>
+        @forelse($coursesStatisticsPage->items() as $item)
+{{--            {{dd($course)}}--}}
+            <tr>
+                <td class="td-title">{{$item->getCourse()->title}}</td>
+                <td class="td-description">{{ $item->getAssignmentsCount() }}</td>
+                <td class="td-author">{{$item->getPassedCount()}}</td>
+            </tr>
+        @empty
+            Курсы отсутствуют
+        @endforelse
+        </tbody>
+    </table>
+    <p class="p-after-table">
+        {{ $coursesStatisticsPage->withQueryString()->links('pagination::bootstrap-5') }}
+    </p>
+    <a class="tu-ul" href="{{ route('courses.list') }}">К списку курсов</a>
+</div>
+</body>
+</html>
+<style>
+    html {
+        background: #AFFAAF;
+    }
+    p {
+        font-size: 24px;
+        color: #2E6243;
+    }
+    a {
+        font-size: 24px;
+        color: #2E6243;
+    }
+    a.tu-ul {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        color: #2E6243;
+        margin-left: 650px;
+    }
+    button {
+        font-size: 24px;
+        height: 50px;
+        padding: 0.5rem 1rem;
+        border-radius: 10px;
+        border-color: black;
+        text-align: center;
+        color: #FFFFFF;
+        background: #2E6243;
+    }
+    button:hover {
+        opacity: 80%;
+    }
+    input {
+        font-size: 24px;
+        width: 500px;
+        height: 45px;
+        border-radius: 5px;
+        margin-left: 30px;
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+    h1 {
+        font-size: 46px;
+        color: #2E6243;
+        margin-left: 120px;
+    }
+    table, th, td {
+        table-layout: fixed;
+        font-size: 24px;
+        border-collapse: collapse;
+        border: 3px solid grey;
+        text-align: center;
+    }
+    button.btn-del-course, button.btn-edit-course {
+        width: 270px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+    div.container-top {
+        background: white;
+        margin-left: 120px;
+        width: 1560px;
+        height: 380px;
+    }
+    div.container-bot {
+        background: white;
+        margin-left: 120px;
+        width: 1560px;
+    }
+    div.btn-group {
+        text-align: right;
+        /*margin-bottom: 200px;*/
+    }
+    a.a-show-for {
+        margin-left: 600px;
+    }
+    a.link-clear-filters {
+        margin-left: 720px;
+    }
+    button.btn-download-json, button.btn-load-json, button.btn-add-course, button.btn-add-file {
+        width: 400px;
+    }
+    p.p-first-part, p.p-second-part, p.p-third-part {
+        margin-bottom: -15px;
+        margin-right: 10px;
+    }
+</style>
